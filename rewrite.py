73157f1f5a68623af642e4b2ccc9c59abf426a0d
Kathleen Keating <kathleen_keating@hms.harvard.edu>
2022-02-11 17:15:29 -0500

    add in skeleton of alt allele check function. most of it still TBD

--- rewrite.py
+++ rewrite.py
@@ -21,6 +21,7 @@ def read_file(filename, hash=False):
 				fields = line.split('\t')
 				# build key from Chromosome #, Loci, Primer
 				key = f"{fields[2]}_{fields[3]}_{fields[0]}"
+				print(f"{fields[2]}_{fields[3]}_{fields[0]}")
 				temp_struct[key] = {}
 				temp_struct[key]["ref"] = fields[5]
 				temp_struct[key]["alt"] = fields[6]
@@ -49,9 +50,71 @@ def alt_allele_check(design_file, allele_object_to_check):
 		- ALT, total DP, AAF, %nt QC
 	"""
 
-	
+	for row in allele_object_to_check.index:
+		# need chr, loci (site1), ref, alt, primer (primer_in_batch)
 
+		# verify with design file
+		# print(f"{allele_object_to_check['chromosome'][row]}_{allele_object_to_check['site1'][row]}_{allele_object_to_check['primer_in_batch'][row]}")
 
+		# chromosome_site1_primer which is the unique key in design_file
+		to_check =  f"{allele_object_to_check['chromosome'][row]}_{allele_object_to_check['site1'][row]}_{allele_object_to_check['primer_in_batch'][row]}"
+
+		# in the design file
+		if to_check in design_file:
+			#print("The following data exists in the design file:", allele_object_to_check.loc[row,:])
+
+			# need first character of 'alt' from allele_object_to_check. This'll be the primary alt: 
+			primary_alt = allele_object_to_check['alt'][row][0]
+			reference_in_allele_file = allele_object_to_check['ref'][row]
+			allele_field = allele_object_to_check['alt'][row]
+
+			# compare primary alt to reference
+			reference_in_design_file =  design_file[to_check]['ref']
+			expected_alt = design_file[to_check]['alt']
+
+			# verify reference in this line is the same as in the design file:
+			if reference_in_allele_file == reference_in_design_file:
+
+				print(primary_alt, " ", expected_alt, " ", reference_in_design_file) #TODO: remove after debugging
+
+				# string match to see if we have the expected alt allele
+				# (relevant when identifying expected alt allele in non-primary position)
+				pos_of_expected_alt_match = allele_field.find(expected_alt)
+
+				# check if ref was called
+				if allele_field == "<*>":
+					print("reference match") #TODO: remove after debugging. Not tested yet
+					#TODO: save to some data structure
+
+				# was the expected alt allele called?
+				elif primary_alt == expected_alt:
+					print("primary alt match") #TODO: remove after debugging. Not tested yet
+					#TODO: save to some data structure
+
+				# was the expected allele called at the non-primary position?
+				elif pos_of_expected_alt_match != -1:
+					print("expected alt match in non-primary position") #TODO: remove after debugging. Not tested yet
+					#TODO: need to modify altreads, dpreads, aaf, pass_read_fraction, and position of match in alt
+					#TODO: save to some data structure
+				
+				# was a non-expected alt allele called?
+				elif pos_of_expected_alt_match == -1:
+					print("non-expected alt allele called") #TODO: remove after debugging. Not tested yet
+					#TODO: need to modify altreads, dpreads, aaf, pass_read_fraction, and position of match in alt
+					#TODO: save to some data structure
+
+				else: 
+					print("ERROR: Exception in logic found in alt_allele_check function due to", allele_object_to_check.loc[row,:])
+
+			# reference is NOT the same as in the design file, which shouldn't ever happen.
+			else:
+				print("ERROR: Reference listed for chromosome, loci, primer ", to_check, "does not match the reference ", reference_in_design_file, "in the design file.")
+				#TODO: need to verify this prints appropriately.
+
+		# not in the design file, which shouldn't ever happen.	
+		else:
+			print("ERROR: The following data is missing in the design file:", allele_object_to_check.loc[row,:])
+			#TODO: need to verify this prints appropriately.
 
 
 if __name__ == "__main__":
@@ -61,16 +124,16 @@ if __name__ == "__main__":
 	parser.add_argument("--oligo_target_file", 
 							required=True, 
 							help="file containing list of oligos targeting which sites, tab-delimited")
-	parser.add_argument("--allele_corrected_file", 
+	parser.add_argument("--corrected_allele_file", 
 							required=True, 
 							help="allele file (error corrected), tab-delimited")
-	parser.add_argument("--allele_uncorrected_file", 
+	parser.add_argument("--uncorrected_allele_file", 
 							required=True, 
 							help="allele file (not error corrected), tab-delimited")
-	parser.add_argument("--50nt_corrected_file", 
+	parser.add_argument("--corrected_50nt_file", 
 							required=True, 
 							help="50nt file (error corrected), tab-delimited")
-	parser.add_argument("--50nt_uncorrected_file", 
+	parser.add_argument("--uncorrected_50nt_file", 
 							required=True, 
 							help="50nt file (not error corrected), tab-delimited")
 	parser.add_argument("--sample_name", 
@@ -89,10 +152,10 @@ if __name__ == "__main__":
 
 	design_file_dict = read_file(args.oligo_target_file, hash=True)
 
-	uncorrected_allele = read_file(args.allele_uncorrected_file)
-	corrected_allele = read_file(args.allele_uncorrected_file)
-	uncorrected_50nt = read_file(args.50nt_uncorrected_file)
-	corrected_50nt = read_file(args.50nt_corrected_file)
+	uncorrected_allele = read_file(args.uncorrected_allele_file)
+	corrected_allele = read_file(args.uncorrected_allele_file)
+	uncorrected_50nt = read_file(args.uncorrected_50nt_file)
+	corrected_50nt = read_file(args.corrected_50nt_file)
 
 	
 	# start pre-processing/filtering step
@@ -110,7 +173,7 @@ if __name__ == "__main__":
 	#TODO: handle output_dir creation/management
 	if not Path(args.output_dir).is_dir():
 		Path(args.output_dir).mkdir()
-	elif 
+	#elif 
 	
 	
 
